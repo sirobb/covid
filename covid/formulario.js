34b84deed9df5056698675f32c3339ae3a136ce1
Boris Alarcón <boris.alarco@gmail.com>
2020-04-11 10:05:44 -0400

    segundo modelo

---
+++ covid/formulario.js
@@ -0,0 +1,9 @@
+var rut = document.getElementById("rut");
+var boton = document.getElementById("botoncito");
+
+
+function click(){
+    location.href ="formulario.html";
+}
+
+boton.addEventListener("click", click);
